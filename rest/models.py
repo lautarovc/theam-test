088583a07dd6661a273d55db5f36a898d7603287
lautarovc <casper1994@gmail.com>
2020-07-21 14:49:11 -0400

    Initial Configuration w/Drive Storage

---
+++ rest/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.dispatch import receiver
+from gdstorage.storage import GoogleDriveStorage
+
+# Define Google Drive Storage
+gd_storage = GoogleDriveStorage()
+
+#----- Models -----#
+
+class Customer(models.Model):
+	"""
+	Contains the customer's information and relation with users
+	"""
+
+	# Required Fields
+	name = models.CharField(max_length=30, verbose_name=('First Name'))
+	surname = models.CharField(max_length=30, verbose_name=('Last Name'))
+	customerId = models.CharField(max_length=30, verbose_name=('Id'))
+
+	# Optional Fields
+
+	# The photo associated with this field will be deleted on_change and on_delete by the django_cleanup module that uses those receivers
+	photo = models.ImageField(storage=gd_storage, height_field=None, width_field=None, max_length=100)
+
+	# Relations
+	createdBy = models.ForeignKey(User, related_name="created_by",on_delete=models.SET_NULL, null=True)
+	lastUpdatedBy = models.ForeignKey(User, related_name="updated_by", on_delete=models.SET_NULL, null=True)
