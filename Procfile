fb7ab1c7aa1bde19455a4673d544937ccb5a06e8
lautarovc <casper1994@gmail.com>
2020-07-23 18:09:56 -0400

    Adds Heroku compatibility

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn theamTest.wsgi --log-file -
--- theamTest/settings.py
+++ theamTest/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -156,3 +157,6 @@ OAUTH2_PROVIDER = {
 #GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = os.environ.get('DRIVE_FILE','')
 GOOGLE_DRIVE_STORAGE_JSON_KEY_FILE = None
 GOOGLE_DRIVE_STORAGE_MEDIA_ROOT = 'media'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
