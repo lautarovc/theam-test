cda82a2616eda003d063ef282cfac50c998ed18a
lautarovc <casper1994@gmail.com>
2020-08-08 19:07:54 -0400

    Fixes id update on customers

--- rest/serializers.py
+++ rest/serializers.py
@@ -38,7 +38,38 @@ class CustomerSerializer(serializers.ModelSerializer):
 		validated_data['lastUpdatedBy'] = self.context['request'].user
 		return super(CustomerSerializer, self).create(validated_data)
 
-	# Assigns current user to the lastUpdatedBy attribute
+	# Reimplemented update to allow editing of id field
 	def update(self, instance, validated_data):
+		# Assigns current user to the lastUpdatedBy attribute
 		validated_data['lastUpdatedBy'] = self.context['request'].user
-		return super(CustomerSerializer, self).update(instance, validated_data)
+
+		serializers.raise_errors_on_nested_writes('update', self, validated_data)
+		info = serializers.model_meta.get_field_info(instance)
+
+		old_instance = Customer.objects.get(pk=instance.id)
+
+		# Simply set each attribute on the instance, and then save it.
+		# Note that unlike `.create()` we don't need to treat many-to-many
+		# relationships as being a special case. During updates we already
+		# have an instance pk for the relationships to be associated with.
+		m2m_fields = []
+		for attr, value in validated_data.items():
+			if attr in info.relations and info.relations[attr].to_many:
+				m2m_fields.append((attr, value))
+			else:
+				setattr(instance, attr, value)
+		instance.save()
+
+		# This is done because editing the id creates another instance, NOT recommended
+		# for more complex models where customer is a foreign key to another table
+		if (old_instance.id != instance.id):
+			old_instance.delete()
+
+		# Note that many-to-many fields are set after updating instance.
+		# Setting m2m fields triggers signals which could potentially change
+		# updated instance and we do not want it to collide with .update()
+		for attr, value in m2m_fields:
+			field = getattr(instance, attr)
+			field.set(value)
+
+		return instance
